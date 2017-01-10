ea4bc75107c80c6f9885385a1f1c8e1c6dc67878
Anthony FAVIER <anthony.favier03@gmail.com>
2017-01-10 11:04:36 +0100

    maj

---
+++ script/script.js
@@ -0,0 +1,9 @@
+Function nombre(){//--------------------nom de la fonction
+var nb = 1//							entier
+
+for (var nb < 200;  nb++) {
+	document.getElementById("liste").textContent="nb";
+}
+}
+
+nombre();
