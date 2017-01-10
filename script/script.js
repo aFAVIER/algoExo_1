b4733b8389eea3b02a9999d10975598f9c9e13ba
Anthony FAVIER <anthony.favier03@gmail.com>
2017-01-10 15:25:14 +0100

    ok

--- script/script.js
+++ script/script.js
@@ -1,9 +1,36 @@
-Function nombre(){//--------------------nom de la fonction
-var nb = 1//							entier
+/*function nom(){//----------------------------------------------------------nom de la fonction
+const SEUIL = 200;//
+var nb;//-------------------------------------------------------------------------entier
 
-for (var nb < 200;  nb++) {
-	document.getElementById("liste").textContent="nb";
+
+for (var nb = 1; nb <= SEUIL; nb++) {//-------------------------------------------boucle For
+	//document.getElementById("liste").innerHTML = "<li>"+nb+"</li>";
+	//document.write("<li>"+nb+"</li>");
+	document.getElementById("liste").innerHTML+="<li>"+nb+"</li>";
+
 }
 }
+nom();*/
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
-nombre();
+function nom(){//----------------------------------------------------------nom de la fonction
+var SEUIL;//
+var nb;//-------------------------------------------------------------------------entier
+
+var SEUIL=prompt("Combien de cases voulez-vous?(en entier et inférieur à 500)");
+if (SEUIL>501|SEUIL!=parseInt(SEUIL)) {
+	alert("C'est pas bon, essaye encore")
+} 
+ 
+else {
+
+
+		for (var nb = 1; nb <= SEUIL; nb++) {//-------------------------------------------boucle For
+			//document.getElementById("liste").innerHTML = "<li>"+nb+"</li>";
+			//document.write("<li>"+nb+"</li>");
+			document.getElementById("liste").innerHTML+="<li>"+nb+"</li>";
+
+		}
+	}
+}
+nom();
